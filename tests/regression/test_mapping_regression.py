2c5e891b04644c81c60c3d95e7116c477a75cae5
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-04-14 13:30:55 +0200

    Add regression test with full set of variables

--- tests/regression/test_mapping_regression.py
+++ tests/regression/test_mapping_regression.py
@@ -2,21 +2,40 @@
 Regression tests of our mapping to branded variables
 """
 
+from pathlib import Path
+
+import pandas as pd
+
 from cmip_branded_variable_mapper.mapper import map_to_cmip_branded_variable
+
+HERE = Path(__file__).parent
 
 
 def test_map_to_cmip_branded_variable(data_regression):
-    branded_variable = map_to_cmip_branded_variable(
-        variable_name="tas",
-        cell_methods="mean where air",
-        dimensions=("time", "lat", "lon"),
-    )
+    TEST_CASES_FILE = HERE.parent / "test-data" / "CMIP7-variables-for-branding.csv"
 
-    reg_check = {
-        "variable_name": "tas",
-        "cell_methods": "mean where air",
-        "dimensions": ("time", "lat", "lon"),
-        "branded_variable": branded_variable,
-    }
+    raw_test_cases = pd.read_csv(TEST_CASES_FILE)
 
-    data_regression.check(reg_check)
+    res_l = []
+    for _, row in raw_test_cases.iterrows():
+        dimensions = tuple([v.strip() for v in row["Dimensions"].split(",")])
+        variable_name = row["Physical Parameter"]
+        cell_methods = row["Cell Methods"]
+
+        branded_variable = map_to_cmip_branded_variable(
+            variable_name=variable_name,
+            cell_methods=cell_methods,
+            dimensions=dimensions,
+        )
+
+        reg_check = {
+            "variable_name": variable_name,
+            "cell_methods": cell_methods,
+            "dimensions": dimensions,
+            "branded_variable": branded_variable,
+        }
+
+        res_l.append(reg_check)
+
+    res_l.sort(key=lambda x: x["branded_variable"])
+    data_regression.check(res_l)
